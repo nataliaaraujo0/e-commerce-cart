2cde894e1e2333b176e67b29858c22da103ea66a
alucard <devjrteste@gmail.com>
2021-11-14 02:34:33 -0300

    chore: provendo store para todo o app

--- ecommerce-cart/src/App.tsx
+++ ecommerce-cart/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ola</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>ola</h1>
+      </div>
+    </Provider>
   );
 }
 
