bfd0d6cc351621f0c889fb2adc896cc42b40a0a4
alucard <devjrteste@gmail.com>
2021-11-14 02:50:43 -0300

    chore: acessando store

--- ecommerce-cart/src/components/Catalog.tsx
+++ ecommerce-cart/src/components/Catalog.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useStore } from 'react-redux';
 
 const Catalog: React.FC = () => {
+
+    const store = useStore();
+
+    console.log(store.getState());
+
     return (
         <h1>Catalog</h1>
     );
