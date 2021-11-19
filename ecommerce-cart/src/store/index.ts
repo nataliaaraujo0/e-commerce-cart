f8d867f7cac0b8e040beb033a4e7d74c2c6fe228
alucard <devjrteste@gmail.com>
2021-11-19 00:23:13 -0300

    chore: Configurando Redux Saga

--- ecommerce-cart/src/store/index.ts
+++ ecommerce-cart/src/store/index.ts
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import { ICartState } from "./modules/cart/types";
-import rootReducer from "./modules/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./modules/cart/rootSaga";
+import rootReducer from "./modules/rootReducer";
 
 export interface IState {
   cart: ICartState;
--- ecommerce-cart/src/store/modules/cart/rootSaga.ts
+++ ecommerce-cart/src/store/modules/cart/rootSaga.ts
@@ -2,6 +2,6 @@ import { all } from "redux-saga/effects";
 
 import cart from "../cart/saga";
 
-export default function* rootSaga() {
+export default function* rootSaga(): Generator {
   return yield all([cart]);
 }
