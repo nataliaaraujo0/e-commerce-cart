bbdabdfa3029a106385fcb901d9b75dad06b42f1
alucard <devjrteste@gmail.com>
2021-11-20 00:28:41 -0300

    chore: verificacao / obtendo quantidade do carrinho

--- ecommerce-cart/src/store/modules/cart/saga.ts
+++ ecommerce-cart/src/store/modules/cart/saga.ts
@@ -1,10 +1,21 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, takeLatest, select } from "redux-saga/effects";
+import { IState } from "../..";
 import { addProductToCart } from "./actions";
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
 
-function checkProductStock({ payload }: CheckProductStockRequest) {
-  console.log(payload);
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find(item => item.product.id === product.id)?.quantity ??
+      0
+    );
+  });
+
+  console.log(currentQuantity);
+  console.log("add ao carrinho");
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
